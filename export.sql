e83f95f559f012ec5f328e63c3b8d035e1f2cf24
Sergey Semushin <s.semushin@innopolis.university>
2020-02-25 15:22:13 +0300

    Updated export script so that table names are not hardcoded

--- export.sql
+++ export.sql
@@ -7,18 +7,16 @@
 
 
 
-COPY category TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/category.csv' DELIMITER ',' CSV HEADER;
-COPY film_category TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/film_category.csv' DELIMITER ',' CSV HEADER;
-COPY film TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/film.csv' DELIMITER ',' CSV HEADER;
-COPY language TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/language.csv' DELIMITER ',' CSV HEADER;
-COPY film_actor TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/film_actor.csv' DELIMITER ',' CSV HEADER;
-COPY inventory TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/inventory.csv' DELIMITER ',' CSV HEADER;
-COPY rental TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/rental.csv' DELIMITER ',' CSV HEADER;
-COPY payment TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/payment.csv' DELIMITER ',' CSV HEADER;
-COPY staff TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/staff.csv' DELIMITER ',' CSV HEADER;
-COPY actor TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/actor.csv' DELIMITER ',' CSV HEADER;
-COPY customer TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/customer.csv' DELIMITER ',' CSV HEADER;
-COPY address TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/address.csv' DELIMITER ',' CSV HEADER;
-COPY city TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/city.csv' DELIMITER ',' CSV HEADER;
-COPY country TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/country.csv' DELIMITER ',' CSV HEADER;
-COPY store TO 'C:/Users/kezzyhko/Desktop/DMD/CSV/store.csv' DELIMITER ',' CSV HEADER;
+DO $$
+DECLARE
+	row RECORD;
+	path VARCHAR;
+BEGIN 
+	FOR row IN
+		SELECT tablename FROM pg_tables WHERE schemaname='public'
+	LOOP
+        path := concat('C:/Users/kezzyhko/Desktop/DMD/CSV/', row.tablename, '.csv');
+        EXECUTE format('COPY %I TO %L WITH (FORMAT CSV, HEADER, FORCE_QUOTE *);', row.tablename, path);
+	END LOOP;
+END;
+$$;
